1d817fb6f654aed280deb05146d6bcdb3f0961ff
bmz <1181625207@qq.com>
2020-01-11 16:30:18 +0800

    第一天的代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 放置数据的地方,初始化的时候直接将用户信息给我们的公共状态
   state: {
+    user: auth.getUser()
   },
   mutations: {
+    updatedUser (state, payload) {
+      state.user = payload.user// 更新数据
+      auth.setUser()// 将数据同步设置到本地存储中
+    },
+    // 清空user
+    clearUser (state) {
+      state.user = {}
+      auth.delUser()// 将缓存中的数据也清空
+    }
   },
   actions: {
   },
